2d17b63237be659a7747096b6e7270081ea446df
SRidez_app <adibayubzai1@gmail.com>
2025-10-07 22:53:25 -0500

    test again

--- src/app/test-quote/page.tsx
+++
@@ -1,9 +0,0 @@
-import QuoteFormTest from "@/Components/quoteformtest";
-
-export default function TestQuotePage() {
-  return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <QuoteFormTest />
-    </div>
-  );
-}
