3b6ee5f197f89a7c5c297c7d82b0eefc96d343f7
SRidez_app <adibayubzai1@gmail.com>
2025-10-09 13:36:42 -0500

    try again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import Image from "next/image";
-import Header from "@/Main/Header";
-import Hero from "@/Main/Hero";
-import Overivew from "@/Main/Overivew";
-import ServiceAreas from "@/Main/serviceAreas";
-import CallToAction from "@/Components/callToAction";
+// import Image from "next/image";
+// import Header from "@/Main/Header";
+// import Hero from "@/Main/Hero";
+// import Overivew from "@/Main/Overivew";
+// import ServiceAreas from "@/Main/serviceAreas";
+// import CallToAction from "@/Components/callToAction";
 
-import Footer from "@/Components/footer";
+// import Footer from "@/Components/footer";
 
-
+import ComingSoon from "../pages/comingsoon";
 export default function Home() {
   return (
     <>
-
-      <Header />
+    <ComingSoon />
+      {/* <Header />
       <Hero />
       <Overivew />
       <ServiceAreas />
       <CallToAction />
       <Footer />
-  
+   */}
     </>
   );
 }
